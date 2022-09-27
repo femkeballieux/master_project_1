4fbfea40e1dee5196f722fa6702a6ddb283dc77e
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-09-27 14:00:03 +0200

    Update crossmatch_radio_surveys.py
    crossmatch party

--- code_martje/crossmatch_radio_surveys.py
+++ code_martje/crossmatch_radio_surveys.py
@@ -17,12 +17,16 @@ import os
 #First was removed?
 
 #When working on laptop
-os.system('C:/Users/Femke/Documents/GitHub/master_project_1/code_martje/topcat-full.jar -stilts \
-          tmatchn matcher=sky multimode=pairs nin=7 params=15 \
-    in1=C:/Users/Femke/Documents/GitHub/master_project_1/data/unresolved_isolated_S_source.fits  values1="RA DEC" \
-    in2=C:/Users/Femke/Documents/GitHub/master_project_1/data/surveys/NVSS.fits values2="RAJ2000 DEJ2000" \
-    in3=C:/Users/Femke/Documents/GitHub/master_project_1/data/surveys/TGSS.fits values3="RAJ2000 DEJ2000" \
-    in4=C:/Users/Femke/Documents/GitHub/master_project_1/data/surveys/VLSSr.fits values4="RAJ2000 DEJ2000" \
-    in5=C:/Users/Femke/Documents/GitHub/master_project_1/data/surveys/LoLSS.fits values5="RA DEC"\
-    in6=C:/Users/Femke/Documents/GitHub/master_project_1/data/surveys/inband_spec_LoTSS.fits values7="RA DEC" \
-    out=C:/Users/Femke/Documents/GitHub/master_project_1/data/crossmatch_NVSS_TGSS_VLSSr_LoLSS_inband.fits')
+os.system('java -jar topcat-full.jar -stilts \
+          tmatchn matcher=sky multimode=pairs nin=6 params=15 \
+    in1=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\unresolved_isolated_S_source.fits  values1="RA DEC" \
+    in2=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\NVSS.fits values2="RAJ2000 DEJ2000" \
+    in3=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\TGSS.fits values3="RAJ2000 DEJ2000" \
+    in4=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\VLSSr.fits values4="RAJ2000 DEJ2000" \
+    in5=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\LoLSS.fits values5="RA DEC"\
+    in6=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\inband_spec_LoTSS.fits values6="RA DEC" \
+    out=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\crossmatch_NVSS_TGSS_VLSSr_LoLSS_inband.fits')
+
+
+
+print("yey we did it")
