d54f1629fd8636a644edfc0423e853d8f61da82c
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-10-04 14:18:06 +0200

    Update change_columnames.py

--- code/change_columnames.py
+++ code/change_columnames.py
@@ -5,15 +5,15 @@ change a column name in a fits file
 from astropy.table import Table
 from astropy.io import fits
 
-file = 'C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\LoLSS_new.fits'
+file = 'C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\crossmatch_NVSS_TGSS_VLSSr_LoLSS_inband.fits'
 survey = fits.open(file, memmap=True)
 data = Table(survey[1].data)
 survey.close()
 
 print('hoi')
-data['Total_flux'].name = 'Total_flux_LoLLS'
-data['E_Total_flux'].name = 'E_Total_flux_LoLLS'
-data['Peak_flux'].name = 'Peak_flux_LoLLS'
-data['E_Peak_flux'].name = 'E_Peak_flux_LoLLS'
+data['Total_flux_LoLLS'].name = 'Total_flux_LoLSS'
+data['E_Total_flux_LoLLS'].name = 'E_Total_flux_LoLSS'
+data['Peak_flux_LoLLS'].name = 'Peak_flux_LoLSS'
+data['E_Peak_flux_LoLLS'].name = 'E_Peak_flux_LoLSS'
 
-data.write('C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\LoLLS_new_names.fits', format='fits', overwrite = True)
+data.write('C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\crossmatch_NVSS_TGSS_VLSSr_LoLSS_inband.fits', format='fits', overwrite = True)
