a2b16e0cebbaaa453bae9daf8d1eda3db60f83ab
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-10-04 12:52:24 +0200

    Update 3_crossmatch_radio_surveys.py

--- code/3_crossmatch_radio_surveys.py
+++ code/3_crossmatch_radio_surveys.py
@@ -12,7 +12,7 @@ os.system('java -jar topcat-full.jar -stilts \
     in2=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\NVSS.fits values2="RAJ2000 DEJ2000" \
     in3=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\TGSS.fits values3="RAJ2000 DEJ2000" \
     in4=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\VLSSr.fits values4="RAJ2000 DEJ2000" \
-    in5=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\LoLSS.fits values5="RA DEC"\
+    in5=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\LoLLS_new_names.fits values5="RA DEC"\
     in6=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\first_14dec17.fits values6="RA DEC"\
     in7=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\surveys\\inband_spec_LoTSS.fits values7="RA DEC" \
     out=C:\\Users\\Femke\\Documents\\GitHub\\master_project_1\\data\\crossmatch_NVSS_TGSS_VLSSr_LoLSS_inband.fits')
